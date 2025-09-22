c12f8a0e9b6142e911e478da8f9102f0bb41224a
Ramon Junior Urena Vasquez <ramon_urena2@ucne.edu.do>
2025-09-22 19:16:54 -0400

    Models

---
+++ p1-Ap1-RamonUreña/DAL/Context.cs
@@ -0,0 +1,7 @@
+﻿namespace p1_Ap1_RamonUreña.DAL
+{
+    public class Context
+    {
+
+    }
+}
---
+++ p1-Ap1-RamonUreña/Models/Registro.cs
@@ -0,0 +1,8 @@
+﻿namespace p1_Ap1_RamonUreña.Models
+{
+    public class Registro
+    {
+
+        public int Id { get; set; }
+    }
+}
